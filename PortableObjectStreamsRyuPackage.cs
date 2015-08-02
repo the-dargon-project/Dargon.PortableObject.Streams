dd4d3754cbcc2880d70fc934456990683294c1f8
Warty <ItzWarty@gmail.com>
2015-08-01 23:10:42 -0700

    Add ryu support in libdpo.streams.

---
+++ PortableObjectStreamsRyuPackage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dargon.Ryu;
+
+namespace Dargon.PortableObjects.Streams {
+   public class PortableObjectStreamsRyuPackage : RyuPackageV1 {
+      public PortableObjectStreamsRyuPackage() {
+         Singleton<PofStreamsFactory, PofStreamsFactoryImpl>();
+      }
+   }
+}
---
+++ Tests/RyuPackageTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dargon.Ryu;
+using NMockito;
+using Xunit;
+
+namespace Dargon.PortableObjects.Streams {
+   public class RyuPackageTests : NMockitoInstance {
+      [Fact]
+      public void Run() {
+         var ryu = new RyuFactory().Create();
+         ryu.Setup();
+         AssertTrue(ryu.Get<PofStreamsFactory>() is PofStreamsFactoryImpl);
+      }
+   }
+}
